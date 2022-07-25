d0ac1b1435ae8cbfc0e4e0c8afb947c5eba3837a
Jeffrey Ho <jeff.ho@hotmail.com>
2022-07-25 20:29:19 +0000

    Assertion Function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  const flame = String.fromCodePoint(0x1F525);
+  const scream = String.fromCodePoint(0x1F631);
+  if (actual === expected) {
+    console.log(flame + flame + flame + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(scream + scream + scream + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual(1,3);
+assertEqual('hello', 'hello');
