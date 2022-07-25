6a0a93a9ba32515c919f14e1d9902d51b8094d5d
Jeffrey Ho <jeff.ho@hotmail.com>
2022-07-25 21:17:52 +0000

    tail.js

---
+++ tail.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  const flame = String.fromCodePoint(0x1f525);
+  const scream = String.fromCodePoint(0x1f631);
+  if (actual === expected) {
+    console.log(
+      flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const tail = function(array) {
+  let localArray = array.slice(1);
+  return localArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const example = tail(["Hello"]);
+assertEqual(example[0], undefined); //should return true
+
+const ex2 = tail([]);
+assertEqual(ex2[0], undefined); // should return true
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
