0f552d5d9c6c23f54992fa83b8153384bb6ef8a8
Jeffrey Ho <jeff.ho@hotmail.com>
2022-07-25 20:37:48 +0000

    AssertEqual with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@ const assertEqual = function(actual, expected) {
   const flame = String.fromCodePoint(0x1F525);
   const scream = String.fromCodePoint(0x1F631);
   if (actual === expected) {
-    console.log(flame + flame + flame + "Assertion Passed: " + actual + " === " + expected);
+    console.log(flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(scream + scream + scream + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
