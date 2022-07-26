9863a99940c1820db8ba2eb970381cbb90d22cee
Jeffrey Ho <jeff.ho@hotmail.com>
2022-07-26 22:17:28 +0000

    without function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const assertArraysEqual = function (actual, expected) {
-  const flame = String.fromCodePoint(0x1F525);
-  const scream = String.fromCodePoint(0x1F631);
+  const flame = String.fromCodePoint(0x1f525);
+  const scream = String.fromCodePoint(0x1f631);
   let value;
   for (let i = 0; i < actual.length; i++) {
     if (actual[i] !== expected[i]) {
@@ -10,14 +10,17 @@ const assertArraysEqual = function (actual, expected) {
     }
   }
   if (value === true) {
-    console.log(flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`);
+    console.log(
+      flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`
+    );
   } else {
-    console.log(scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`);
+    console.log(
+      scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`
+    );
   }
 };
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, 3], [3, 2, 1])
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
---
+++ without.js
@@ -0,0 +1,50 @@
+const assertArraysEqual = function (actual, expected) {
+  const flame = String.fromCodePoint(0x1f525);
+  const scream = String.fromCodePoint(0x1f631);
+  let value;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      value = false;
+    } else {
+      value = true;
+    }
+  }
+  if (value === true) {
+    console.log(
+      flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+
+// given two arrays as parameters
+//I want to remove contents of array 2 from array 1
+// output should be an array without the unwanted elements
+//do not mutate the original strings. So no split, join, slice, splice
+
+
+//loop through each value of arr1 and see if it is included within arr2. 
+//if the value of arr1 is not included within arr2, add it to an empty array
+// output would return the resulting array
+
+const without = function(arr1, arr2) {
+  let array = [];
+  for (let i = 0 ; i < arr1.length ; i++) {
+    if (arr2.includes(arr1[i]) === false) {
+      array.push(arr1[i]);
+    }
+  }
+  return array;
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
