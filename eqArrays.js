e2550e4dc6af27b0e7792b2ed4988ddb1677f9b0
Jeffrey Ho <jeff.ho@hotmail.com>
2022-07-26 21:24:31 +0000

    eqArrays.js function added

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+const assertEqual = function (actual, expected) {
+  const flame = String.fromCodePoint(0x1f525);
+  const scream = String.fromCodePoint(0x1f631);
+  if (actual === expected) {
+    console.log(
+      flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`
+    );
+  } else {
+    console.log(
+      scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`
+    );
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let status;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      status = false;
+    } else {
+      status = true;
+    }
+  }
+  return status;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); //false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
