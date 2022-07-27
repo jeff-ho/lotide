48cbeb6197e6fb83bac4dd3e4b3d17a084772256
Jeffrey Ho <jeff.ho@hotmail.com>
2022-07-27 00:26:52 +0000

    tail function

--- tail.js
+++ tail.js
@@ -17,6 +17,7 @@ const tail = function (array) {
   return localArray;
 };
 
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
