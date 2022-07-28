8cb98225379e5a11c2b25cee8dddc736b77179cb
Jeffrey Ho <jeff.ho@hotmail.com>
2022-07-28 00:53:28 +0000

    assertObsEq function with other changes

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,18 +1,4 @@
-const assertEqual = function (actual, expected) {
-  const flame = String.fromCodePoint(0x1f525);
-  const scream = String.fromCodePoint(0x1f631);
-  if (actual === expected) {
-    console.log(
-      flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`
-    );
-  } else {
-    console.log(
-      scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`
-    );
-  }
-};
-
-
+//our eqArrays function
 const eqArrays = function(arr1, arr2) {
   let status;
   if (arr1.length !== arr2.length) {
@@ -28,17 +14,15 @@ const eqArrays = function(arr1, arr2) {
   return status;
 };
 
-//our actual eqObjects function
+//our eqObjects function
 const eqObjects = function(object1, object2) {
   let arr1Length = Object.keys(object1).length
   let arr1Keys = Object.keys(object1)
   let arr2Length = Object.keys(object2).length
   let status;
-
   if (arr1Length !== arr2Length) {
     return false;
   }
-
   for (let key of arr1Keys) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key]) === true) {
       status = eqArrays(object1[key],object2[key])
@@ -53,18 +37,20 @@ const eqObjects = function(object1, object2) {
 };
 
 
+//our actual assetObjectsEqual function
+const assertObjectsEqual = function(actual, expected) {
+const inspect = require('util').inspect;
+const flame = String.fromCodePoint(0x1f525);
+const scream = String.fromCodePoint(0x1f631);
+let status = eqObjects(actual, expected);
+
+if (status === true) {
+  console.log(flame + flame + flame + `Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+} else {
+  console.log(scream + scream + scream + `Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+} 
+};
 
 
-const ab = { a: "1", b: "2"};
-const ba = { b: "2", a: "1"};
-console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
