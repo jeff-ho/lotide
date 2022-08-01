e5f88c0dc4ee3e89c9880b4495d0f3a724b2f753
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-01 23:15:25 +0000

    refactored map

--- map.js
+++ map.js
@@ -1,42 +1,3 @@
-const assertArraysEqual = function (actual, expected) {
-  const flame = String.fromCodePoint(0x1f525);
-  const scream = String.fromCodePoint(0x1f631);
-  let value;
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      value = false;
-    } else {
-      value = true;
-    }
-  }
-  if (value === true) {
-    console.log(
-      flame + flame + flame + `Assertion Passed: ${actual} === ${expected}`
-    );
-  } else {
-    console.log(
-      scream + scream + scream + `Assertion Failed: ${actual} !== ${expected}`
-    );
-  }
-};
-
-const eqArrays = function (arr1, arr2) {
-  let status;
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      status = false;
-    } else {
-      status = true;
-    }
-  }
-  return status;
-};
-
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function (array, callback) {
   const results = [];
   for (let item of array) {
@@ -44,4 +5,5 @@ const map = function (array, callback) {
   }
   return results;
 };
+
 module.exports = map;
