c27222622d5148419d4598c7ee69ca979f416967
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-01 17:56:45 +0000

    refactored assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -13,8 +13,6 @@ const assertEqual = function (actual, expected) {
 };
 
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 3);
-assertEqual("hello", "hello");
+module.exports = assertEqual;
+
+
