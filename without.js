edf9acabbddfd14581563fac9173bc6103e80079
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-01 18:00:45 +0000

    test with new head

--- without.js
+++ without.js
@@ -47,3 +47,6 @@ console.log(without([1, 2, 3], [1])); // => [2, 3]
 // without(words, ["lighthouse"]); // no need to capture return value for this test case
 // // Make sure the original array was not altered by the without function
 // assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+
